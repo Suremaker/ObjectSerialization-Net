549a4e083e754b0bd9aa31811db6a2b5db457cea
suremaker <wojciech.kotlarski@gmail.com>
2021-10-12 23:54:00 +0100

    #2 Upgraded .net40 to .net461 and dependencies

--- ObjectSerialization.Performance/Serializers/NewtonBsonAdapter.cs
+++ ObjectSerialization.Performance/Serializers/NewtonBsonAdapter.cs
@@ -13,13 +13,13 @@ namespace ObjectSerialization.Performance.Serializers
         public string Name { get { return "NewtonBSON"; } }
         public T Deserialize<T>(Stream stream, out long operationTime)
         {
-            using (var reader = new BsonReader(stream) { CloseInput = false })
+            using (var reader = new BsonDataReader(stream) { CloseInput = false })
                 return ExecutionTimer.Measure(() => _serializer.Deserialize<T>(reader), out operationTime);
         }
 
         public void Serialize<T>(Stream stream, T value, out long operationTime)
         {
-            using (var writer = new BsonWriter(stream) { CloseOutput = false })
+            using (var writer = new BsonDataWriter(stream) { CloseOutput = false })
             {
                 ExecutionTimer.Measure(() => _serializer.Serialize(writer, value), out operationTime);
             }
